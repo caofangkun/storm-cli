fd5add229c1c1716b2f42723c09ee989547ce725
caofangkun <caofangkun@gmail.com>
2015-04-07 15:36:57 +0800

    init project storm-cli

---
+++ src/main/java/com/tencent/jstorm/ql/parse/SemanticAnalyzer.java
@@ -0,0 +1,11 @@
+package com.tencent.jstorm.ql.parse;
+
+import backtype.storm.topology.TopologyBuilder;
+
+public class SemanticAnalyzer extends BaseSemanticAnalyzer {
+
+  public SemanticAnalyzer(TopologyBuilder builder) {
+    super(builder);
+  }
+
+}
